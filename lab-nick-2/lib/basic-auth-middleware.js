481ba0c401cd93b65b868dfd5a54e286fd802fc0
dambergn <dambergn@gmail.com>
2018-03-28 15:49:57 -0700

    finished basic-auth-middleware.js

--- lab-nick-2/lib/basic-auth-middleware.js
+++ lab-nick-2/lib/basic-auth-middleware.js
@@ -12,4 +12,25 @@ module.exports = function(req, res, next) {
   };
 
   var base64str = authHeader.split('Basic ')[1];
+  if(!base64str) {
+    return next(createError(401, 'username and password required'));
+  };
+
+  var utf8str = Buffer.from(base64str, 'base64').toString();
+  var authArr = utf8str.split(':');
+
+  req.auth = {
+    username: authArr[0],
+    password: authArr[1]
+  };
+
+  if (!req.auth.username) {
+    return next(createError(401, 'username required'));
+  };
+
+  if (!req.auth.password) {
+    return next(createError(401, 'password required'));
+  };
+
+  next();
 };
